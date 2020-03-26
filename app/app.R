847bcfa2ca2009843c45955744fc7ddc71dd5c57
Sam Clifford <sj.clifford@gmail.com>
2020-03-26 16:50:28 +0000

    adding an acknowledgements tab #22

--- app/app.R
+++ app/app.R
@@ -99,7 +99,11 @@ ui <- navbarPage(
   )),
   tabPanel("Information", 
            fluidPage(style="padding-left: 40px; padding-right: 40px; padding-bottom: 40px;", 
-                     includeMarkdown("info.md")))
+                     includeMarkdown("info.md"))),
+  tabPanel("Acknowledgements", 
+           fluidPage(style="padding-left: 40px; padding-right: 40px; padding-bottom: 40px;", 
+                     includeMarkdown("ack.md")))
+  
 )
 
 
