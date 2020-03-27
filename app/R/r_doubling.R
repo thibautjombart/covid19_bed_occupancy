1bd847ab26c56748cc124b4af7bb93fa0a012523
Thibaut Jombart <thibautjombart@gmail.com>
2020-03-27 19:17:33 +0000

    cosmetic changes

--- app/R/r_doubling.R
+++ app/R/r_doubling.R
@@ -1,10 +1,12 @@
 #' Generate random values of doubling time
 #'
-#' Values are generated from a normal distribution.
+#' Values are generated from a inverse Gamma distribution.
+#'
+#' @author Samuel Clifford
 #'
 
-r_doubling <- function(mean, cv, n) {
-    # parameterised in terms of an inverse gamma to avoid truncating at 0
+r_doubling <- function(n, mean, cv) {
+    ## parameterised in terms of an inverse gamma to avoid truncating at 0
     
     if (cv == 0){return(rep(x = mean, times = n))} else{
         invgamma::rinvgamma(n = n, shape = 2 + 1/cv^2, rate = mean*(1 + 1/cv^2))    
