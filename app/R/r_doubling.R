6d49de1bc27db05776bcaa608c42bbece659433c
Thibaut Jombart <thibautjombart@gmail.com>
2020-03-27 16:15:27 +0000

    switch to inverse gamma

--- app/R/r_doubling.R
+++ app/R/r_doubling.R
@@ -4,5 +4,10 @@
 #'
 
 r_doubling <- function(mean, cv, n) {
-  rnorm(n, mean, cv * mean)
+    # parameterised in terms of an inverse gamma to avoid truncating at 0
+    
+    if (cv == 0){return(rep(x = mean, times = n))} else{
+        invgamma::rinvgamma(n = n, shape = 2 + 1/cv^2, rate = mean*(1 + 1/cv^2))    
+    }
+    
 }
